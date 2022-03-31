e4b287994eaf4589a297dde31a763890f44c2656
Cheddargt <gus_ghz@live.com>
2022-03-31 00:27:03 -0300

    projeto atualizado

--- rescue-test/main.py
+++ rescue-test/main.py
@@ -60,34 +60,57 @@ def getAmbiente():
 
     return ambienteDict
 
-def printMap(map):
+def printMap(map, agent_pos, ag_vasc_visited):
     print("----------------- mapa atual -----------------")
     for line in map:
         print("     ",line)
     print("--------------- end mapa atual ---------------")
+    print("agent_pos: ", agent_pos)
+    print("ag_vasc_visited: ", ag_vasc_visited)
+    print("----------------------------------------------")
+    
+    # time.sleep(0.2)
 
-def calcularRetorno(agent_init_pos, agent_pos, ag_vasc_visited):
+def calcularRetorno(agent_init_pos, agent_pos, ag_vasc_visited, bateria_vasc, caminho_retorno, hora_de_voltar):
     """Retorna uma lista [] em que [0] é uma bool que informa se é hora de voltar\n
         e [1] é o caminho de volta\n
         true = volta"""
-    
+
     ## objetivo é a posição inicial do agente, do começo do programa
     goal = agent_init_pos
+    current_test_pos = agent_pos
+    bateria_pra_voltar = 0
+    caminho_temp = [];
 
-    caminho = []
+    pos_n = [agent_pos[0] + movePos["N"][0], agent_pos[1] + movePos["N"][1]]
+    pos_o = [agent_pos[0] + movePos["O"][0], agent_pos[1] + movePos["O"][1]]
+    pos_l = [agent_pos[0] + movePos["L"][0], agent_pos[1] + movePos["L"][1]]
+    pos_s = [agent_pos[0] + movePos["S"][0], agent_pos[1] + movePos["S"][1]]
+    pos_ne = [agent_pos[0] + movePos["NE"][0], agent_pos[1] + movePos["NE"][1]]
+    pos_no = [agent_pos[0] + movePos["NO"][0], agent_pos[1] + movePos["NO"][1]]
+    pos_se = [agent_pos[0] + movePos["SE"][0], agent_pos[1] + movePos["SE"][1]]
+    pos_so = [agent_pos[0] + movePos["SO"][0], agent_pos[1] + movePos["SO"][1]]
+
     menorCusto = INFINITE
-    hora_de_voltar = False
 
-    return caminho, hora_de_voltar
+    caminho_temp.append(agent_pos)
 
-def tentarMover(agent_pos, mov_dir, map, ag_vasc_walls, ag_vasc_vict):
+    ## enquanto a última posição do vetor não for o objetivo (não encontrou o caminho)
+    while caminho_temp[len(caminho_temp)-1] != goal and bateria_pra_voltar+2 < bateria_vasc: 
+        for pos in reversed(ag_vasc_visited):
+            if (pos != agent_pos):
+                caminho_temp.append(pos)
+                bateria_pra_voltar += 1
+        
+    if (bateria_pra_voltar+2 >= bateria_vasc): 
+        hora_de_voltar = True
 
-    time.sleep(1)
-    printMap(map)
-    print("agent_pos: ", agent_pos)
-    print("vitimas encontradas: ", ag_vasc_vict)
+    caminho_retorno = caminho_temp;
 
-    
+    return hora_de_voltar
+        
+def tentarMover(agent_pos, agent_init_pos, mov_dir, map, bateria_vasc, 
+ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar):
     new_pos = [agent_pos[0] + movePos[mov_dir][0], agent_pos[1] + movePos[mov_dir][1]]
     x, y = new_pos[0], new_pos[1]
 
@@ -103,9 +126,11 @@ def tentarMover(agent_pos, mov_dir, map, ag_vasc_walls, ag_vasc_vict):
     if (map[x][y] == 3):
         # gasta 1 de bateria
         # gasta 1 de tempo
-        map[x][y] = 4 # parede visitada
+        # parede visitada
+        map[x][y] = 4 
         ag_vasc_walls.append([x, y])
 
+        # printMap(map, agent_pos, ag_vasc_visited)
         # não se move
         return 
     
@@ -113,11 +138,22 @@ def tentarMover(agent_pos, mov_dir, map, ag_vasc_walls, ag_vasc_vict):
         # gasta 1 de bateria
         # gasta 1 de tempo
         map[x][y] = -1 # posição visitada
+
+        # if (calcularRetorno(agent_init_pos, agent_pos, 
+        # ag_vasc_visited, bateria_vasc, caminho_retorno, hora_de_voltar)):
+        #     return
+        
+        print ("variável: ", "agent_pos", "//", "valor: ", agent_pos, "//", "tipo: ", type(agent_pos))
         agent_pos = [x, y]
-        tentarMover(agent_pos, "L", map, ag_vasc_walls, ag_vasc_vict)
-        tentarMover(agent_pos, "S", map, ag_vasc_walls, ag_vasc_vict)
-        tentarMover(agent_pos, "N", map, ag_vasc_walls, ag_vasc_vict)
-        tentarMover(agent_pos, "O", map, ag_vasc_walls, ag_vasc_vict)
+        print ("variável: ", "agent_pos", "//", "valor: ", agent_pos, "//", "tipo: ", type(agent_pos))
+        ag_vasc_visited.append(agent_pos)
+
+        printMap(map, agent_pos, ag_vasc_visited)
+
+        tentarMover(agent_pos, agent_init_pos, "S", map, bateria_vasc, ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
+        tentarMover(agent_pos, agent_init_pos, "L", map, bateria_vasc, ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
+        tentarMover(agent_pos, agent_init_pos, "N", map, bateria_vasc, ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
+        tentarMover(agent_pos, agent_init_pos, "O", map, bateria_vasc, ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
 
     ## encontrou uma vítima
     if (map[x][y] == 1):
@@ -126,12 +162,30 @@ def tentarMover(agent_pos, mov_dir, map, ag_vasc_walls, ag_vasc_vict):
         # gasta 1 de tempo
         # scaneia gastando tempo e bateria
         map[x][y] = 2 # posição visitada
-        agent_pos = [x, y]
-        tentarMover(agent_pos, "S", map, ag_vasc_walls, ag_vasc_vict)
-        tentarMover(agent_pos, "L", map, ag_vasc_walls, ag_vasc_vict)
-        tentarMover(agent_pos, "O", map, ag_vasc_walls, ag_vasc_vict)
-        tentarMover(agent_pos, "N", map, ag_vasc_walls, ag_vasc_vict)
 
+        # if (calcularRetorno(agent_init_pos, agent_pos, 
+        #     ag_vasc_visited, bateria_vasc, caminho_retorno)):
+        #     return
+        
+        print ("variável: ", "agent_pos", "//", "valor: ", agent_pos, "//", "tipo: ", type(agent_pos))
+        agent_pos = [x, y]
+        print ("variável: ", "agent_pos", "//", "valor: ", agent_pos, "//", "tipo: ", type(agent_pos))
+
+        ag_vasc_visited.append(agent_pos)
+
+        printMap(map, agent_pos, ag_vasc_visited)
+
+        tentarMover(agent_pos, agent_init_pos, "S", map, bateria_vasc, ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
+        tentarMover(agent_pos, agent_init_pos, "L", map, bateria_vasc, ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
+        tentarMover(agent_pos, agent_init_pos, "N", map, bateria_vasc, ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
+        tentarMover(agent_pos, agent_init_pos, "O", map, bateria_vasc, ag_vasc_walls, ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
+
+    # gasta 1 de bateria
+    # gasta 1 de tempo
+
+    printMap(map, agent_pos, ag_vasc_visited)
+
+    return
 
     ## se o movimento for na diagonal
     ## if (0,0) (1,1)
@@ -146,38 +200,11 @@ def tentarMover(agent_pos, mov_dir, map, ag_vasc_walls, ag_vasc_vict):
     #     return False
     
 
-    
-
-# def dfs(visitados, ag_vasc_visited, pos_atual):
-#     if pos_atual not in visitados:
-#         print (pos_atual)
-#         visitados.add(pos_atual)
-#         for neighbour in ag_vasc_visited[pos_atual]:
-#             dfs(visitados, ag_vasc_visited, neighbour)
-
-
-
-# def dfs(self):
-#         """
-#         DFS: Busca em profundidade
-#             Retorna a próxima coordenada não visitada
-#         """
-#         for direction in ["NW", "NE", "SW", "SE", "N", "W", "E", "S"]:
-#             x, y = self.directionCoord(direction)
-#             if self.checkValidCoord(x, y) and not self.visited[y][x]:
-#                 self.stack.append((x, y))
-
-#         # Próxima coordenada não visitada da pilha
-#         while self.stack:
-#             x, y = self.stack.pop(-1)
-#             if not self.visited[y][x]:
-#                 return x, y
-#         return None, None
-
 def explorarMapa(agent_init_pos, map, ag_vasc_visited, 
     bateria_vasc, ag_vasc_walls, ag_vasc_vict):
 
-    agent_pos = [0, 0]
+    agent_pos = [0,0]
+    print ("variável: ", "agent_pos", "//", "valor: ", agent_pos, "//", "tipo: ", type(agent_pos))
 
     ## "visita" a posição inicial do agente
     map[agent_init_pos[0]] [agent_init_pos[1]] = -1
@@ -189,19 +216,52 @@ def explorarMapa(agent_init_pos, map, ag_vasc_visited,
     ## adiciona posição visitada a um vetor de posições visitadas  
     ag_vasc_visited.append(agent_init_pos)
 
+    bateria_vasc = 5
+
+    caminho_retorno = []
+    hora_de_voltar = False
+
     ## verifica se possui bateria pra voltar + 2 (pra escanear uma vítima)
-    ## calcularRetorno 
-    while not (calcularRetorno(agent_init_pos, agent_pos, ag_vasc_visited)[0]):
-        tentarMover(agent_pos, "S", map, ag_vasc_walls, ag_vasc_vict)
-        printMap(map)
-        print("agent_pos: ", agent_pos)
+    tentarMover(agent_pos, agent_init_pos, "S", map, bateria_vasc, ag_vasc_walls, 
+        ag_vasc_vict, ag_vasc_visited, caminho_retorno, hora_de_voltar)
 
-        time.sleep(1)
-        
+    # print ("variável: ", "caminho_retorno", "//", "valor: ", caminho_retorno, "//", "tipo: ", type(caminho_retorno))
+    print ("variável: ", "caminho_retorno", "//", "valor: ", caminho_retorno, "//", "tipo: ", type(caminho_retorno))
 
-    ## retorna pra base pelo melhor caminho
+    # ## retorna pra base pelo melhor caminho
+    # for pos in caminho_retorno:
+    #     # print ("variável: ", "pos", "//", "valor: ", pos, "//", "tipo: ", type(pos))
+    #     map[pos[0]][pos[1]] = -2
+    #     bateria_vasc -= 1
+    #     print ("variável: ", "agent_pos", "//", "valor: ", agent_pos, "//", "tipo: ", type(agent_pos))
+    #     # agent_pos = pos
+    #     print ("variável: ", "agent_pos", "//", "valor: ", agent_pos, "//", "tipo: ", type(agent_pos))
 
-    
+    return
+
+def criarPlanoResgate(map, ag_vasc_vict, ag_vasc_visited, plano_resgate):
+    ## encontrar a vítima mais próxima
+    ## inicializa vítima mais próxima, 
+    closest_victim = []
+    ## inicializa custo até a vítima mais próxima
+    closest_victim_cost = INFINITE
+    ## inicializa caminho parcial
+    caminho_parcial = []
+
+    ## calcula distância euclidiana da base até todas as vítimas
+    ## determina objetivo parcial com base no resultado
+    ## primeira vítima = mais perto
+    ## encontra caminho de menor custo até ela
+        ## aqui que tá o problema
+    ## adiciona caminho de menor custo ao caminho parcial
+    ## repete até nenhum outro caminho ser encontrado
+    ## adiciona caminho de menor custo ao plano_resgate
+    ## define a vítima como "posição atual"
+    ## reseta vítima mais próxima
+    ## reseta custo da vítima mais próxima
+    ## reseta caminho parcial
+
+
 
 
 def main():
@@ -220,9 +280,6 @@ def main():
 
     ## cria o mapa
     map = [[0 for col in range(configDict["maxLin"])] for row in range(configDict["maxCol"])]
-    
-    ## imprime o mapa
-    printMap(map)
 
     print("dicionario ambiente: ", ambienteDict)
 
@@ -244,16 +301,27 @@ def main():
         map[i][j] = 1
 
     ## alterar o mapa dentro da função altera fora também, é ponteiro
+    ## mapa é passado apenas para fins de verificação dentro das regras
+    ## dos sensores do agente vasculhador
     explorarMapa(agent_init_pos, map, ag_vasc_visited, 
         bateria_vasc, ag_vasc_walls, ag_vasc_vict)
 
+    printMap(map, ag_vasc_vict, ag_vasc_visited)
+
     ## após a exploração do mapa
     ## ag_vasc_visited terá uma ordem de visitação do agente vasculhador
-    ag_vasc_visited =   [[0,0], [1,0], [2,0], [3,0], [4,0], 
-                        [4,1], [4,2], [4,3], [4,4], [3,4],
-                        [2,4], [1,4], [0,4], [1,3], [2,3],
-                        [3,3], [3,2], [2,2], [3,1], [1,1]
-                        [0,1]]
+    
+    # ag_vasc_visited =   [[0,0], [1,0], [2,0], [3,0], [4,0], 
+    #                     [4,1], [4,2], [4,3], [4,4], [3,4],
+    #                     [2,4], [1,4], [0,4], [1,3], [2,3],
+    #                     [3,3], [3,2], [2,2], [3,1], [1,1]
+    #                     [0,1]]
+
+    ag_vasc_visited =   [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], 
+                         [4, 1], [4, 2], [4, 3], [4, 4], [3, 4], 
+                         [2, 4], [1, 4], [0, 4], [1, 3], [2, 3], 
+                         [3, 3], [3, 2], [2, 2], [3, 1], [1, 1], 
+                         [0, 1]]
 
     ## ag_vasc_walls terá um vetor com as paredes encontradas
 
@@ -265,12 +333,14 @@ def main():
 
     ## calcular o melhor caminho
 
+    plano_resgate = []
+
+    criarPlanoResgate(map, ag_vasc_vict, ag_vasc_visited, ag_vasc_walls, plano_resgate)
+
     ## executar
 
     ## voltar pra base
 
-    printMap(map);
-    print("ag_vasc_visited: ", ag_vasc_visited)
 
     
 
